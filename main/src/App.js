de792949059041e748657c9402c817dc74240a9a
Victoria Benoit <vrose834@gmail.com>
2023-11-08 12:55:18 -0400

    Finished the navbar

--- main/src/App.js
+++ main/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+     <Navbar />
     </div>
   );
 }
---
+++ main/src/Components/Navbar.js
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+
+    return ( 
+        <nav>
+            <h1>Reddit</h1>
+            <div>
+                <input></input>
+                <button>Search</button>
+            </div>
+        </nav>
+
+     );
+}
+ 
+export default Navbar;
